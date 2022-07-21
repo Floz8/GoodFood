e7160b613d85249d79d0c9a96e059b9c57591258
zappone2u <arthzappone@hotmail.fr>
2022-07-21 09:52:54 +0100

    new design view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,63 +2,78 @@
 
 @section('content')
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <style>
+        .bg-glass {
+        background-color: hsla(0, 0%, 100%, 0.7) !important;
+        backdrop-filter: saturate(200%) blur(5px);
+        }
+  </style>
 <x-guest-layout>
     <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-               
-            </a>
-        </x-slot>
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+        <div class="row d-flex justify-content-center align-items-center h-500">
+            <div class="col-md-9 col-lg-6 col-xl-5">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Sample image">
             </div>
+            <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1 card bg-glass">
+                <div class="card-body px-4 py-5 px-md-5">
+                    <x-slot name="logo">
+                        <a href="/">
+                        
+                        </a>
+                    </x-slot>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+                    <!-- Validation Errors -->
+                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <!-- Name -->
+                        <div>
+                            <x-label for="name" :value="__('Name')" />
+
+                            <x-input id="name" class="form-control form-control-lg" placeholder="Entrez votre nom" type="text" name="name" :value="old('name')" required autofocus />
+                        </div>
+
+                        <!-- Email Address -->
+                        <div class="mt-4">
+                            <x-label for="email" :value="__('Email')" />
+
+                            <x-input id="email" class="form-control form-control-lg" placeholder="Entrez votre mail" type="email" name="email" :value="old('email')" required />
+                        </div>
+
+                        <!-- Password -->
+                        <div class="mt-4">
+                            <x-label for="password" :value="__('Password')" />
+
+                            <x-input id="password" class="form-control form-control-lg" placeholder="Entrez votre mot de passe"
+                                            type="password"
+                                            name="password"
+                                            required autocomplete="new-password" />
+                        </div>
+
+                        <!-- Confirm Password -->
+                        <div class="mt-4">
+                            <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                            <x-input id="password_confirmation" class="form-control form-control-lg" placeholder="Confirmez votre mot de passe"
+                                            type="password"
+                                            name="password_confirmation" required />
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <x-button class="btn btn-primary btn-lg" style="padding-left: 2.5rem; margin-right: 5rem; margin-bottom: 5rem; ">
+                                {{ __('Register') }}
+                            </x-button>
+
+                            <a class="underline text-sm text-gray-600 hover:text-gray-900" style="margin-bottom: 5rem;" href="{{ route('login') }}">
+                                {{ __('Already registered?') }}
+                            </a>
+                        </div>
+                    </form>
+                </div>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
+        </div>
     </x-auth-card>
 </x-guest-layout>
 @endsection
