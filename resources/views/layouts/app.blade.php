f11df88c60bd20ccc3e3b69420d2e5f26d08c7d2
Floz8 <55977382+Floz8@users.noreply.github.com>
2022-07-21 11:28:37 +0200

    Redirection

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,9 +36,7 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="/">Dashboard <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="register">Créer un compte</a>
-      <a class="nav-item nav-link" href="login">Connexion</a>
+ 
     </div>
   </div>
 </nav>
--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -1,10 +1,17 @@
 @extends('layouts.layout2')
 
 @section('content')
-<body><h1>{{$restaurant->Nom}}</h1></body>
-@foreach($restaurant->plats as $plat)
-<div>{{$plat->Nom}}      {{$plat->prix}}€   <a href="{{route('restaurants.show', ['id' => $restaurant->id])}}" class="btn btn-primary">Ajouter au panier</a></div> 
 
+<body><h1>{{$restaurant->Nom}}</h1></body>
+
+@foreach($restaurant->plats as $plat)
+@if (Auth::check())
+<div>{{$plat->Nom}}      {{$plat->prix}}€   <a href="{{route('restaurants.show', ['id' => $restaurant->id])}}" class="btn btn-primary">Ajouter au panier</a></div> 
+@else
+<div>{{$plat->Nom}}      {{$plat->prix}}€   <a href="../login" class="btn btn-primary">Ajouter au panier</a></div> 
+@endif
 
 @endforeach
+
+
 @endsection
